eb3101a435bf2a0d85ee3f32cdacf8793a59d49a
Ricardo Pires <ricardoxp@gmail.com>
2022-07-17 19:55:35 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,38 +1,30 @@
-package com.devsuperior.dsmeta.controllers;
+package com.devsuperior.dsmeta.services;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
-import com.devsuperior.dsmeta.services.SaleService;
-import com.devsuperior.dsmeta.services.SmsService;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
 
-@RestController
-@RequestMapping(value = "/sales")
-public class SaleController {
+@Service
+public class SaleService {
 
 	@Autowired
-	private SaleService service;
+	private SaleRepository repository;
 	
-	@Autowired
-	private SmsService smsService;
-	
-	@GetMapping
-	public Page<Sale> findSales(
-			@RequestParam(value="minDate", defaultValue = "") String minDate, 
-			@RequestParam(value="maxDate", defaultValue = "") String maxDate, 
-			Pageable pageable) {
-		return service.findSales(minDate, maxDate, pageable);
-	}
-	
-	@GetMapping("/{id}/notification")
-	public void notifySms(@PathVariable Long id) {
-		smsService.sendSms(id);
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable); 
 	}
 }
